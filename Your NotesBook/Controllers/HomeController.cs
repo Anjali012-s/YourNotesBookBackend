6d74e740b0d1465589b91b2b68bffb73e85328bc
Anjali012 <anjalisharma@crescerance.com>
2022-10-10 22:23:45 +0530

    add controller

--- Your NotesBook/Controllers/HomeController.cs
+++ Your NotesBook/Controllers/HomeController.cs
@@ -28,5 +28,9 @@ namespace Your_NotesBook.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public void CheckingGit()
+        {
+             
+        }
     }
 }
